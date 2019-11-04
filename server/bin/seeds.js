13f25ae008da7693621ac9031408fb6ad2b0a142
chris bolivar <christopherbolivar@hotmail.com>
2019-11-03 21:19:55 -0500

    pagination fully works, readjust self according to the amount of apps in db

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -4,25 +4,28 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') })
 const mongoose = require('mongoose')
 const App = require('../models/App')
 
-const bcryptSalt = 10
-
 require('../configs/database')
 
-let appArray = []
-for (let i = 1; i <= 100; i++) {
-  appArray.push({
-    id: Number(i),
-    name: `my-app-${i}`,
-  })
+let generateJSON = () => {
+  let appArray = []
+  for (let i = 1; i <= 200; i++) {
+    appArray.push({
+      id: Number(i),
+      name: `my-app-${i}`,
+    })
+  }
+  return appArray
 }
+console.log(generateJSON(), '====')
 
 App.deleteMany()
   .then(() => {
-    return App.create(appArray)
+    return App.create(generateJSON())
   })
   .then(appCreated => {
-    console.log(`${appCreated.length} apps created with the following id:`)
-    console.log(appCreated.map(u => u._id))
+    // console.log(`${appCreated.length} apps created with the following id:`)
+    // console.log(appCreated.map(u => u._id))
+    console.log('success!')
   })
   .then(() => {
     // Close properly the connection to Mongoose
@@ -30,5 +33,5 @@ App.deleteMany()
   })
   .catch(err => {
     mongoose.disconnect()
-    throw err
+    throw err + 'error '
   })
